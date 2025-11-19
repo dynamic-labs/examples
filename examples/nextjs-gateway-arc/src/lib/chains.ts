50f2bdf59cac786df8e194216971c20ed634874a
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2025-11-20 00:49:08 +1100

    chore: some updates

--- examples/nextjs-gateway-arc/src/lib/chains.ts
+++ examples/nextjs-gateway-arc/src/lib/chains.ts
@@ -1,15 +1,3 @@
-import { defineChain } from "viem";
-export const arcTestnet = defineChain({
-  id: 5042002,
-  name: "Arc Testnet",
-  network: "arc-testnet",
-  nativeCurrency: { decimals: 18, name: "Ether", symbol: "ETH" },
-  rpcUrls: { default: { http: ["https://rpc.testnet.arc.network"] } },
-  blockExplorers: {
-    default: { name: "Arc Scan", url: "https://testnet.arcscan.app" },
-  },
-});
-
 export const DOMAIN = {
   sepolia: 0,
   baseSepolia: 6,
